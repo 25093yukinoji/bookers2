b1a5497f0e76edc456a59a00468452fab1e20835
25093yukinoji <ten.yuki.0601@gmail.com>
2020-12-26 10:14:01 +0000

    [Add]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,22 +1,43 @@
 class BooksController < ApplicationController
 
   def new
-    @book = Book.new
+    @book = Book.new(book_params)
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to books_path
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
-    @books = Book.all
+    @books = Book.page(params[:page]).reverse_order
+    @book = Book.new
   end
 
   def show
     @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book =Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root to: 'books#index'
   resources :books, only: [:new, :create, :index, :show, :destroy]
 
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit, :update]
 end
